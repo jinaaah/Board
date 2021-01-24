579640130117666cd2a1117a23d81a163c72a79b
zozza <zozza@DESKTOP-0OKC7A5>
2021-01-25 00:44:47 +0900

    board insert, select

---
+++ Board/src/main/java/com/board/domain/BoardDTO.java
@@ -0,0 +1,47 @@
+package com.board.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class BoardDTO {
+
+	/** 번호 (PK) */
+	private Long idx;
+
+	/** 제목 */
+	private String title;
+
+	/** 내용 */
+	private String content;
+
+	/** 작성자 */
+	private String writer;
+
+	/** 조회 수 */
+	private int viewCnt;
+
+	/** 공지 여부 */
+	private String noticeYn;
+
+	/** 비밀 여부 */
+	private String secretYn;
+
+	/** 삭제 여부 */
+	private String deleteYn;
+
+	/** 등록일 */
+	private LocalDateTime insertTime;
+
+	/** 수정일 */
+	private LocalDateTime updateTime;
+
+	/** 삭제일 */
+	private LocalDateTime deleteTime;
+
+}
